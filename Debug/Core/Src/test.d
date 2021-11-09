16823e3c71753a1e344193020de9db6bbeb07b47
Maria Halvarsson <mahalv@kth.se>
2021-11-09 13:58:52 +0100

    added Show_points function

--- Debug/Core/Src/test.d
+++ Debug/Core/Src/test.d
@@ -1,3 +1,6 @@
-Core/Src/test.o: ../Core/Src/test.c ../Core/Inc/Test.h
+Core/Src/test.o: ../Core/Src/test.c ../Core/Inc/Test.h \
+ ../Core/Inc/Pingpong.h
 
 ../Core/Inc/Test.h:
+
+../Core/Inc/Pingpong.h:
