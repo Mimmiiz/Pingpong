ed167ee054aaaf07c0ec9b1aaa82e68598eb8317
hamta <hamta@kth.se>
2021-11-09 13:48:37 +0100

    Show points function done

--- Core/Src/pingpong_functions.c
+++ Core/Src/pingpong_functions.c
@@ -62,3 +62,66 @@ void Led_on(uint8_t Lednr) {
 	} // end for-loop
 	return;
 } // End of functions Led_on
+
+void setLed(uint8_t led) {
+		if (led==1) HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
+		if (led==2) HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+		if (led==3) HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
+		if (led==4) HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, GPIO_PIN_SET);
+		if (led==5) HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, GPIO_PIN_SET);
+		if (led==6) HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, GPIO_PIN_SET);
+		if (led==7) HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, GPIO_PIN_SET);
+		if (led==8) HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, GPIO_PIN_SET);
+		return;
+}
+
+void resetLed (uint8_t led) {
+		if (led==1) HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+		if (led==2) HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+		if (led==3) HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_RESET);
+		if (led==4) HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, GPIO_PIN_RESET);
+		if (led==5) HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, GPIO_PIN_RESET);
+		if (led==6) HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, GPIO_PIN_RESET);
+		if (led==7) HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, GPIO_PIN_RESET);
+		if (led==8) HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, GPIO_PIN_RESET);
+		return;
+}
+
+void setLeds(void) {
+	uint8_t s = 1;
+	while(s <= 8) {
+		setLed(s);
+		s++;
+	}
+
+	return;
+}
+
+void resetLeds(void) {
+	uint8_t r = 1;
+	while(r <= 8) {
+		resetLed(r);
+		r++;
+	}
+
+	return;
+}
+
+/**
+@brief Show_points, shows points after missed ball
+@param1 uint8_t L_points points for left player
+@param2 uint8_t R_points points for right player
+@return void, no return value
+ */
+
+void Show_points(uint8_t L_points, uint8_t R_points) {
+	for (uint8_t i = 1; i <= L_points; i++) {
+		setLed(i);
+		HAL_Delay(10);
+	}
+	for(uint8_t j = 8; j > 8-R_points; j--) {
+		setLed(j);
+		HAL_Delay(10);
+	}
+	return;
+}
